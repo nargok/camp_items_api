9428e8e925da3c1a75e5b5d872b4df25adf5d272
nargok <nargok@gmail.com>
2020-08-30 20:47:28 +0900

    express settings

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("hello ts!");
+import express from "express";
+
+const PORT: number = 3000;
+const app: express.Express = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const router: express.Router = express.Router();
+
+router.get("/api/getTest", (req: express.Request, res: express.Response) => {
+  res.send(req.query);
+});
+
+app.use(router);
+app.listen(PORT, () => {
+  console.log(`Camp item API listening on port ${PORT}!`);
+});
